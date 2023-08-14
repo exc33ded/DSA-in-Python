6985d452bc4fa070b9269ea5c95480ffa3d8fafd
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-08-14 19:08:39 +0530

    Delete Element_wise_sum.py

--- Tuple/Excersice/Element_wise_sum.py
+++
@@ -1,4 +0,0 @@
-# Create a function that takes two tuples and returns a tuple containing the element-wise sum of the input tuples.
-
-def tuple_elementwise_sum(tuple1, tuple2):
-    return tuple(map(sum, zip(tuple1, tuple2)))
