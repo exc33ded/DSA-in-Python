b689c1bec7bde334ea4743ccbcb1254cd29494d1
Mohammed Sarim <sarimisraiely12@gmail.com>
2023-09-04 23:11:20 +0530

    These are the methods which we can follow to create a linked List commit

--- Linked List/intializing.py
+++ Linked List/intializing.py
@@ -12,6 +12,14 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
         
+# -------- OR -----------
+
+class LinkedList_:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        
 new_linked_list = LinkedList(10)
 print(new_linked_list) # <__main__.LinkedList object at 0x000001BC1F18BD50>
 print(new_linked_list.head.value) # 10
