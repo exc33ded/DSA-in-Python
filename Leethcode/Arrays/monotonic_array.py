817cb06a7d23faa3141e4886f85157fea003d666
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-09-10 23:00:31 +0530

    Update monotonic_array.py

--- Leethcode/Arrays/monotonic_array.py
+++ Leethcode/Arrays/monotonic_array.py
@@ -1,1 +1,12 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        increase = True
+        decrease = True
 
+        for i in range(0, (len(nums)-1)):   # O(n)
+            if (nums[i] < nums[i+1]):  #O(1)
+                decrease = False
+            if (nums[i] > nums[i+1]): # O(1)
+                increase = False
+            
+        return increase or decrease
