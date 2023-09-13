10f941ff4513720fe52a0e57166835aa4e24c04b
Mohammed Sarim <82305547+exc33ded@users.noreply.github.com>
2023-09-13 22:50:53 +0530

    Create shuffle_array.py

---
+++ Leethcode/Arrays/shuffle_array.py
@@ -0,0 +1,12 @@
+# 1470. Shuffle the Array
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        lst = []
+        for i in range(n):
+            lst.append(nums[i])
+            lst.append(nums[n])
+
+            n += 1
+
+        return lst
