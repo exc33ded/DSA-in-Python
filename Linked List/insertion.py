afd8a2903b1157af1c9150de1d8aa5fbf600f75e
Mohammed Sarim <sarimisraiely12@gmail.com>
2023-10-13 23:32:44 +0530

    commit

--- Linked List/insertion.py
+++ Linked List/insertion.py
@@ -6,7 +6,7 @@ class Node:
         self.next = None
         
 class LinkedList:
-    def __inint__(self):
+    def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
@@ -17,4 +17,14 @@ class LinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+new_LL = LinkedList()
+new_LL.append(10)
+new_LL.append(20)
+
+print(new_LL.head.value)
+print(new_LL.tail.value)
+print(new_LL.length)
