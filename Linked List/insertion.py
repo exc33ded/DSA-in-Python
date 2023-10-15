060d78455296e85180949c6a2d3435ff14506930
Mohammed Sarim <sarimisraiely12@gmail.com>
2023-10-15 11:52:05 +0530

    commit

--- Linked List/insertion.py
+++ Linked List/insertion.py
@@ -11,6 +11,16 @@ class LinkedList:
         self.tail = None
         self.length = 0
         
+    def __str__(self):
+        temp_node = self.head
+        result = " "
+        while temp_node is not None:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += " -> "
+            temp_node = temp_node.next
+        return result
+    
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -21,10 +31,14 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
+        
 new_LL = LinkedList()
 new_LL.append(10)
 new_LL.append(20)
+new_LL.append(30)
+new_LL.append(40)
 
 print(new_LL.head.value)
 print(new_LL.tail.value)
 print(new_LL.length)
+print(new_LL)
