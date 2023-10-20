946bd20f744d63774cabbed4ac37b2e7bebf2635
Mohammed Sarim <sarimisraiely12@gmail.com>
2023-10-20 22:01:24 +0530

    commit

--- Linked List/traversing.py
+++ Linked List/traversing.py
@@ -11,9 +11,25 @@ class LinkedList:
         
     def append(self, value):
         new_node = Node(value)
-        if new_node is None:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
+        self.length += 1
+    
+    def traverse(self):
+        current = self.head
+        while current is not None:
+            print(current.value)
+            current = current.next
+            
+new_ll = LinkedList()
+new_ll.append(10)
+new_ll.append(20)
+new_ll.append(30)
+new_ll.append(40)
+new_ll.append(50)
+
+new_ll.traverse()
