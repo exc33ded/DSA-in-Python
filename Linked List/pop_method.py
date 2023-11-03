cfec06567e530933c7c37b8d5515551cf061c93d
Mohammed Sarim <sarimisraiely12@gmail.com>
2023-11-03 21:59:37 +0530

    commit

--- Linked List/pop_method.py
+++ Linked List/pop_method.py
@@ -19,3 +19,29 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
         
+    def show(self):
+        current = self.head
+        while current:
+            print(current.value, end=" ")
+            current = current.next
+            
+    def pop_first(self):
+        popped_node = self.head
+        self.head = self.head.next
+        popped_node.next = None
+        self.length -= 1
+        return popped_node.value
+    
+new_ll = LinkedList()
+new_ll.append(10)
+new_ll.append(20)
+new_ll.append(30)
+new_ll.append(40)
+new_ll.append(50)
+
+new_ll.show()
+print()
+
+print(new_ll.pop_first(), "\n")
+new_ll.show()
+        
