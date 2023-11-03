297d4d82f5466b1ec8c79b2814d140e4393470f8
Mohammed Sarim <sarimisraiely12@gmail.com>
2023-11-03 21:51:57 +0530

    commit

--- Linked List/pop_method.py
+++ Linked List/pop_method.py
@@ -3,3 +3,19 @@ class Node:
         self.value = value
         self.next = None
         
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        
