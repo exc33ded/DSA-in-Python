3610759d4a847619d177e09c616eed8fa9bb5fc5
Mohammed Sarim <sarimisraiely12@gmail.com>
2024-01-11 19:20:39 +0530

    csll

--- Linked List/Circular Linked List/initializing.py
+++ Linked List/Circular Linked List/initializing.py
@@ -4,5 +4,12 @@ class Node:
         self.next = None
         
 class CSLL:
-    def __init__(self):
+    def __init__(self, value):
         new_node = Node(value)
+        new_node.next = new_node
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+        
+csLL = CSLL(10)
+print(csLL.head.value)
